5999fdf0a7edfff79ce68a64aaf83173bba7184d
Diego <diegodecstraid@gmail.com>
2025-06-19 23:01:41 -0300

    Enunciados dos Exercicios 04, 05 e 06

---
+++ exercicios-modulo-1-completo/exercicio-06.js
@@ -0,0 +1,6 @@
+/*
+    6. Memoization
+    Implemente function memoize(fn) que armazene em cache chamadas
+    anteriores de fn (por argumentos), retornando resultados instantâneos em
+    repetidas invocações.
+*/
