8cf24059334b76fbd1e899e1552fd9065e27c787
Diego <diegodecstraid@gmail.com>
2025-06-20 19:24:14 -0300

    Resolução do Exercicio 04

--- exercicios-modulo-1-completo/exercicio-04.js
+++ exercicios-modulo-1-completo/exercicio-04.js
@@ -5,3 +5,28 @@
 */
 
 const prompt = require ('prompt-sync')();
+
+fa
+function fatorial(n) {
+    if (n < 0) {
+        throw new Error("Não existe fatorial de número negativo");
+    }
+
+    if (n === 0) {
+        // Caso base: fatorial(0) = 1
+        return 1;
+    }
+    // Chamada recursiva
+    return n * fatorial(n - 1);
+}
+
+try {
+    console.log(fatorial(5));
+    console.log(fatorial(0));
+    console.log(fatorial(1));
+    console.log(fatorial(-3));
+} catch (erro) {
+    //Mensagem de erro
+    console.log("Erro:", erro.message);
+}
+
