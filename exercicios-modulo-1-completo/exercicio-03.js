ae0f188f7a3de105362d709a7099ce2cbbc42a91
Diego <diegodecstraid@gmail.com>
2025-06-20 19:07:10 -0300

    Resolução do Exercicio 03

--- exercicios-modulo-1-completo/exercicio-03.js
+++ exercicios-modulo-1-completo/exercicio-03.js
@@ -5,3 +5,29 @@
 */
 
 const prompt = require ('prompt-sync')();
+
+let frase = "olá olá grande mundo imenso e azul nesta vasta escuridão espacial";
+
+// separa em array de palavras
+let palavras = frase.split(" ");
+let unicas = [];
+
+for (let i = 0; i < palavras.length; i++) {
+    let palavra = palavras[i];
+    let contador = 0;
+
+    // Conta quantas vezes a palavra aparece no array
+    for (let j = 0; j < palavras.length; j++) {
+        if (palavras[j] === palavra) {
+            contador++;
+        }
+    }
+
+    // Se apareceu só uma vez e ainda não está no array de únicas, adiciona
+    if (contador === 1) {
+        unicas.push(palavra);
+    }
+}
+
+//Resultado
+console.log(unicas);
