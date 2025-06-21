2e8c91037ec95c23b88e36d3afd42e45d112466b
Diego <diegodecstraid@gmail.com>
2025-06-21 14:35:58 -0300

    Resolução do Exercicio 06

--- exercicios-modulo-1-completo/exercicio-06.js
+++ exercicios-modulo-1-completo/exercicio-06.js
@@ -6,3 +6,38 @@
 */
 
 const prompt = require ('prompt-sync')();
+
+function memoize (fn) {
+    let cache = {};
+
+    return function (...args) {
+        // transforma os argumentos em string para usar como chave
+        let key = JSON.stringify(args);
+
+         if (cache[key] !== undefined) {
+            console.log("Resultado vindo do cache");
+            return cache[key];
+        }
+        // calcula
+        let resultado = fn.apply(this,args);
+        // guarda no cache
+        cache[key] = resultado;         
+        return resultado;
+    };
+}
+
+// Declaramos o fatorial como função normal (sem chamar ela diretamente ainda)
+let fatorial = null;
+
+// Cria a função memoizada já atribuindo a fatorial
+fatorial = memoize(function(n) {
+    if (n === 0) return 1;
+    // agora usa a versão memoizada
+    return n * fatorial(n - 1);
+});
+// calcula
+console.log(fatorial(5));
+// cache
+console.log(fatorial(5));
+ // aproveita o cache do 5 para calcular 6
+console.log(fatorial(6));
