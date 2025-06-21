67a31192a0d91b9df69fe5e31bb1fc6f093e6c1e
Diego <diegodecstraid@gmail.com>
2025-06-21 16:57:31 -0300

    Resolução do Exercicio 09

--- exercicios-modulo-1-completo/exercicio-09.js
+++ exercicios-modulo-1-completo/exercicio-09.js
@@ -9,3 +9,31 @@
 */
 
 const prompt = require ('prompt-sync')();
+
+// Converte array de pares em objeto
+function paresParaObjeto (pares) {
+    let obj = {};
+    for (let [chave, valor] of pares) {
+        obj[chave] = valor;
+    }
+    return obj;
+}
+
+// Converte objeto em array de pares
+function objetoParaPares(obj) {
+    let pares = [];
+    for (let chave in obj) {
+        pares.push([chave, obj[chave]]);
+    }
+    return pares;
+}
+
+// Testes
+let arrayDePares = [["nome", "Diego"], ["idade", 30], ["cidade", "São Paulo"]];
+let objeto = { nome: "Diego", idade: 30, cidade: "São Paulo" };
+
+console.log("Pares para objeto:");
+console.log(paresParaObjeto(arrayDePares));
+
+console.log("Objeto para pares:");
+console.log(objetoParaPares(objeto));
