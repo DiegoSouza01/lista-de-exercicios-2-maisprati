40f0e3dfc38d2058a5341ba941c6cf79b6061185
Diego <diegodecstraid@gmail.com>
2025-06-20 23:41:54 -0300

    Resolução do Exercicio 05

--- exercicios-modulo-1-completo/exercicio-05.js
+++ exercicios-modulo-1-completo/exercicio-05.js
@@ -6,3 +6,29 @@
 */
 
 const prompt = require ('prompt-sync')();
+
+function debounce(fn, delay) {
+    let timerId;
+
+    return function (...args) {
+        // Cancela o último timer se a função for chamada antes do delay terminar
+        clearTimeout(timerId);
+
+        // Agenda uma nova chamada da função
+        timerId = setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    };
+}
+
+function escreverMensagem(msg) {
+    console.log("Mensagem enviada:", msg);
+}
+
+let escreverComDebounce = debounce(escreverMensagem, 1000);
+
+// Simulando chamadas rápidas (como digitação)
+escreverComDebounce("olá");
+escreverComDebounce("olá, tudo");
+escreverComDebounce("olá, tudo bem?");
+// Apenas a última será executada após 1 segundo sem novas chamadas
